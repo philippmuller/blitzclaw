54c19e7c1c5b28968f252f9300b55af12d8b4502
Ava <ava@ava-macbook-pro.fritz.box>
2026-02-08 12:26:59 +0100

    Redirect new sign-ups to /onboarding

--- apps/web/src/app/sign-up/[[...sign-up]]/page.tsx
+++ apps/web/src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100">
-      <SignUp afterSignUpUrl="/dashboard" signInUrl="/sign-in" />
+      <SignUp afterSignUpUrl="/onboarding" signInUrl="/sign-in" />
     </div>
   );
 }
